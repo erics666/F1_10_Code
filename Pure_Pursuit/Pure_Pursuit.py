67b3b8fffa0944127117509c77ddcc3508a82b54
erics666 <69947009+erics666@users.noreply.github.com>
2020-08-19 23:22:01 -0400

    Add files via upload

---
+++ Pure_Pursuit/Pure_Pursuit.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from numpy import linalg as la
+import os
+import sys
+import csv
+import math
+import tf
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+
+
+
+# TODO: import ROS msg types and libraries
+pointIndex = 0
+path_points=[] 
+
+dirname  = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'waypoint/wp-test10.csv') 
+with open(filename) as csvfile:
+    data = list(csv.reader(csvfile))
+    path_points_tmp=np.asarray(data)  
+    path_points=path_points_tmp.astype(np.float32) #open and read the csv file
+
+class PurePursuit(object):
+    """
+    The class that handles pure pursuit.
+    """
+    def __init__(self):
+        # TODO: create ROS subscribers and publishers
+        self.pose_msg = rospy.Subscriber('/odom', Odometry, self.pose_callback)
+        self.drive_pub = rospy.Publisher('/nav', AckermannDriveStamped, queue_size=10)
+
+
+    def pose_callback(self, pose_msg):
+        # Import .csv file into a list (path_points)
+        global pointIndex
+        global path_points
+       
+        if pointIndex == len(path_points):
+                pointIndex = 0
+        x = pose_msg.pose.pose.position.x
+        y = pose_msg.pose.pose.position.y
+        
+        #distance between robot and waypoint
+        distance = math.sqrt(math.pow((path_points[pointIndex,0]-x),2)+math.pow((path_points[pointIndex,1]-y),2))
+        if distance >= 1.7:        
+        #select closest waypoint over 1.5 distance away
+            qx=pose_msg.pose.pose.orientation.x
+	    qy=pose_msg.pose.pose.orientation.y
+	    qz=pose_msg.pose.pose.orientation.z
+	    qw=pose_msg.pose.pose.orientation.w
+
+	    quaternion = (qx,qy,qz,qw)
+            euler = tf.transformations.euler_from_quaternion(quaternion)
+            yaw = euler[2]
+            v1 = np.array([np.cos(yaw),np.sin(yaw)])
+            v2 = np.array([path_points[pointIndex,0]-x, path_points[pointIndex,1]-y])
+            angle = self.find_angle(v1, v2) #find angle of waypoint
+            
+            #TODO: calculate curvature/steering angle
+            #if (angle > 0.4189) or (angle < -0.4189):
+            #   angle = (angle/abs(angle))*0.4189 
+
+            # TODO: publish drive message, don't forget to limit the steering angle between -0.4189 and 0.4189 radians
+            drive_msg = AckermannDriveStamped()
+            drive_msg.header.stamp = rospy.Time.now()
+            drive_msg.header.frame_id = "laser"
+	    drive_msg.drive.speed = 1
+	    if (angle > 0.4189) or (angle < -0.4189):
+               angle = (angle/abs(angle))*0.4189 
+               drive_msg.drive.speed = 0.5
+            drive_msg.drive.steering_angle = angle
+            self.drive_pub.publish(drive_msg)
+        else:   # if robot is too close to waypoint
+            pointIndex = pointIndex + 1 # select next waypoint
+            
+    # find the angle bewtween two vectors    
+    def find_angle(self, v1, v2):
+         v1 = v1/np.linalg.norm(v1)
+         v2 = v2/np.linalg.norm(v2)
+         v12 = np.cross(v1,v2)
+         return math.asin(v12)
+
+
+def main():
+    rospy.init_node('pure_pursuit_node')
+    pp = PurePursuit()
+    rospy.spin()
+if __name__ == '__main__':
+    main()
---
+++ Pure_Pursuit/waypoint_display.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+#created by Xiyuan Zhu, Lehigh University, 4/15/2020
+# place thisin the waypoint_logger folder and run after waypoint_logger.py.
+# with simulator running, add the  topic in rviz.
+
+from __future__ import print_function
+import rospy
+
+# TODO: import ROS msg types and libraries
+import sys
+import numpy as np
+import csv
+from os.path import expanduser
+from visualization_msgs.msg import Marker
+from std_msgs.msg import String
+
+def main():
+    rospy.init_node('pure_pursuit_node')
+    home = expanduser('~')
+    filepath=home+'/rcws/logs/wp-test10.csv' 
+    with open(filepath,'r') as csvfile:
+        data = list(csv.reader(csvfile))
+        waypoints_tmp=np.asarray(data)  #tansfer list to array
+        waypoints=waypoints_tmp.astype(np.float32) #transfer string to float 
+    
+    marker_pub = rospy.Publisher('wp', Marker, queue_size=10)
+    marker=Marker()
+    marker_pub.publish(marker)
+
+    speaker_pub = rospy.Publisher('chatter', String, queue_size=10)
+    rate=rospy.Rate(10)
+
+    #self.marker.action=3
+    #self.marker_pub.publish(self.marker)
+    while not rospy.is_shutdown():
+        for i in range(len(waypoints)):
+            marker.ns = "Waypoints"
+            marker.id = i
+            marker.type = Marker.SPHERE
+            marker.header.frame_id = 'map'
+            marker.action = Marker.ADD
+            marker.pose.position.x = waypoints[i,0]
+            marker.pose.position.y = waypoints[i,1]
+            marker.pose.position.z = 0.5
+            marker.pose.orientation.x = 0.0
+            marker.pose.orientation.y = 0.0
+            marker.pose.orientation.z = 0.0
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+            marker.color.a = 1.0 #Don't forget to set the alpha!
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker_pub.publish(marker)
+        rate.sleep()
+
+        #rospy.loginfo(marker)
+        #only if using a MESH_RESOURCE marker type:
+        #self.marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae"
+    rospy.spin()
+        
+    
+    
+if __name__ == '__main__':
+    main()
